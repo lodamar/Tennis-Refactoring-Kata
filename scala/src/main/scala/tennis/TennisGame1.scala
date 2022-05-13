1952eaec83da83c5becf1d4e52c57d42e02c4096
Davide Pavan <davide.pavan@bitrock.it>
2022-05-13 10:54:50 +0200

    Added score class

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -3,11 +3,25 @@ package tennis
 class TennisGame1(val player1Name: String, val player2Name: String) extends TennisGame {
   var points: Points = Points(player1 = player1Name, player2 = player2Name)
 
-  def wonPoint(playerName: String) {
+  def wonPoint(playerName: String): Unit =
     points = points.addOneTo(playerName)
-  }
 
   def calculateScore(): String =
+    Score.scoreFor(points)
+
+}
+
+case class Points(player1: String, points1: Int = 0, player2: String, points2: Int = 0) {
+  val same: Boolean = points1 == points2
+  val deuceTieBreak: Boolean = points1 >= 4 || points2 >= 4
+
+  def addOneTo(player: String): Points =
+    if (player == player1) copy(points1 = points1 + 1)
+    else copy(points2 = points2 + 1)
+}
+
+object Score {
+  def scoreFor(points: Points): String =
     if (points.same) {
       scoreWhenSamePoints(points.points1)
     } else if (points.deuceTieBreak) {
@@ -43,12 +57,4 @@ class TennisGame1(val player1Name: String, val player2Name: String) extends Tenn
       case _ => "Deuce"
     }
 
-  case class Points(player1: String, points1: Int = 0, player2: String, points2: Int = 0) {
-    val same: Boolean = points1 == points2
-    val deuceTieBreak: Boolean = points1 >= 4 || points2 >= 4
-
-    def addOneTo(player: String): Points =
-      if (player == player1) copy(points1 = points1 + 1)
-      else copy(points2 = points2 + 1)
-  }
 }
