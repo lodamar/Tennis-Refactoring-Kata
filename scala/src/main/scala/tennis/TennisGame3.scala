5aeb1f1eec73c00860d1d2940766c35981aa1c38
Davide Pavan <davide.pavan@bitrock.it>
2022-05-13 17:04:26 +0200

    Moved expression into implicit class

--- scala/src/main/scala/tennis/TennisGame3.scala
+++ scala/src/main/scala/tennis/TennisGame3.scala
@@ -5,35 +5,28 @@ class TennisGame3(val p1N: String, val p2N: String) extends TennisGame {
   var p2: Int = 0
   var p1: Int = 0
 
+  def points: (Int, Int) = (p1, p2)
+
   def calculateScore(): String = {
     var s: String = ""
-    if (lestThenFourButNotDeuce) {
+    if (points.lestThenFourButNotDeuce) {
       val p = Array("Love", "Fifteen", "Thirty", "Forty")
       s = p(p1)
-      if (same) s + "-All" else s + "-" + p(p2)
+      if (points.same) s + "-All" else s + "-" + p(p2)
     } else {
-      if (same) "Deuce"
+      if (points.same) "Deuce"
       else {
-        s = if (oneGreaterThanTwo) p1N else p2N
-        if ((difference) * (difference) == 1) "Advantage " + s else "Win for " + s
+        s = if (points.oneGreaterThanTwo) p1N else p2N
+        if (points.difference * points.difference == 1) "Advantage " + s else "Win for " + s
       }
     }
   }
 
-  private def difference = {
-    p1 - p2
-  }
-
-  private def oneGreaterThanTwo = {
-    p1 > p2
-  }
-
-  private def same = {
-    p1 == p2
-  }
-
-  private def lestThenFourButNotDeuce = {
-    p1 < 4 && p2 < 4 && !(p1 + p2 == 6)
+  implicit class __PointsOps(points: (Int, Int)) {
+    val difference: Int = p1 - p2
+    val oneGreaterThanTwo: Boolean = p1 > p2
+    val same: Boolean = p1 == p2
+    val lestThenFourButNotDeuce: Boolean = p1 < 4 && p2 < 4 && !(p1 + p2 == 6)
   }
 
   def wonPoint(playerName: String) {
